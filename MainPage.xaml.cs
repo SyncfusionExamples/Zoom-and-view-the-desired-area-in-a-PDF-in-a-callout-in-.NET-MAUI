768602706245fe1845fe6f8f1797ab20016159ea
ramkumarsync <74192192+ramkumarsync@users.noreply.github.com>
2024-05-07 11:09:25 +0530

    Initial commit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Handlers;
 using Syncfusion.Maui.PdfToImageConverter;
 using Syncfusion.Maui.PdfViewer;
 using System.Reflection;
@@ -14,7 +14,58 @@ namespace PdfZoomCallout
         {
             InitializeComponent();
             PdfViewer.AnnotationSettings.Square.BorderWidth = 1;
-            PdfViewer.DocumentSource = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("PdfZoomCallout.Assets.pdf_succinctly.pdf");
+            PdfViewer.DocumentSource = typeof(App).GetTypeInfo().Assembly.GetManifestResourceStream("PdfZoomCallout.Assets.Pdf_Succinctly.pdf");
+        }
+
+        private void PdfViewer_DocumentLoaded(object sender, EventArgs e)
+        {
+            // Initiate image converter for the document loaded in the PDF Viewer.
+            imageConverter?.Dispose();
+            if (PdfViewer.DocumentSource is Stream documentStream)
+            {
+                documentStream.Position = 0;
+                Stream stream = new MemoryStream();
+                documentStream.CopyTo(stream);
+                stream.Position = 0;
+                imageConverter = new PdfToImageConverter(stream);
+            }
+        }
+
+        private async void CalloutMode_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                // Enable rectangle drawing mode.
+                PdfViewer.AnnotationMode = AnnotationMode.Square;
+                if (showHint)
+                {
+                    // Show hint for zoom and highlight a specific portion.
+                    showHint = await DisplayAlert("Callout mode", "Callout mode is enabled. To zoom and highlight a desired area, draw a rectangle over it.", "OK", "Don't show again");
+                }
+            }
+            else
+            {
+                // Disable the rectangle drawing mode.
+                PdfViewer.AnnotationMode = AnnotationMode.None;
+                // Close callout popup.
+                CalloutImage.Source = null;
+                CalloutPopup.IsVisible = false;
+            }
+        }
+
+        private void PdfViewer_AnnotationAdded(object sender, Syncfusion.Maui.PdfViewer.AnnotationEventArgs e)
+        {
+            // Get the bounds of the rectangle drawn to crop and show the specific portion.
+            Annotation? annotation = e.Annotation;
+            if (annotation != null && annotation is SquareAnnotation squareAnnotation)
+            {
+                float scaleFactor = GetScaleFactor(squareAnnotation.Bounds.Width, squareAnnotation.Bounds.Height);
+                Stream? imageStream = imageConverter?.Convert(annotation.PageNumber - 1,
+                    squareAnnotation.Bounds, scaleFactor);
+                CalloutPopup.IsVisible = true;
+                CalloutImage.Source = ImageSource.FromStream(() => imageStream);
+                PdfViewer.RemoveAnnotation(annotation);
+            }
         }
 
         // Returns the calculated scale factor to show the specific portion without quality loss.
@@ -31,77 +82,11 @@ namespace PdfZoomCallout
             }
         }
 
-        /// <summary>
-        /// Closes the callout popup.
-        /// </summary>
-        void CloseCalloutPopup()
-        {
-            CalloutImage.Source = null;
-            CalloutPopup.IsVisible = false;
-        }
-
-        /// <summary>
-        /// Displays the portion of image from the given source.
-        /// </summary>
-        void ShowCalloutPortion(Stream? source)
-        {
-            CalloutPopup.IsVisible = true;
-            CalloutImage.Source = ImageSource.FromStream(() => source);
-        }
-
-        #region Event handlers
-        private void PdfViewer_DocumentLoaded(object sender, EventArgs e)
-        {
-            // Initiate image converter for the document loaded in the PDF Viewer.
-            imageConverter?.Dispose();
-            if (PdfViewer.DocumentSource is Stream documentStream)
-            {
-                documentStream.Position = 0;
-                Stream stream = new MemoryStream();
-                documentStream.CopyTo(stream);
-                stream.Position = 0;
-                imageConverter = new PdfToImageConverter(stream);
-            }
-        }
-
-        private void PdfViewer_AnnotationAdded(object sender, Syncfusion.Maui.PdfViewer.AnnotationEventArgs e)
-        {
-            // Get the bounds of the rectangle drawn to crop and show the specific portion.
-            Annotation? annotation = e.Annotation;
-            if (annotation != null && annotation is SquareAnnotation squareAnnotation)
-            {
-                float scaleFactor = GetScaleFactor(squareAnnotation.Bounds.Width, squareAnnotation.Bounds.Height);
-                Stream? imageStream = imageConverter?.Convert(annotation.PageNumber - 1,
-                    squareAnnotation.Bounds, scaleFactor);
-                ShowCalloutPortion(imageStream);
-                PdfViewer.RemoveAnnotation(annotation);
-            }
-        }
-
-        private async void Switch_Toggled(object sender, ToggledEventArgs e)
-        {
-            if (e.Value == true)
-            {
-                // Enable rectangle drawing mode.
-                PdfViewer.AnnotationMode = AnnotationMode.Square;
-                if (showHint)
-                {
-                    // Show hint for zoom and highlight a specific portion.
-                    showHint = await DisplayAlert("Zoom and Highlighter", "To zoom and highlight a specific area, draw a rectangle over it.", "OK", "Don't show again");
-                }
-            }
-            else
-            {
-                // Disable the rectangle drawing mode.
-                PdfViewer.AnnotationMode = AnnotationMode.None;
-                // Close callout popup.
-                CloseCalloutPopup();
-            }
-        }
-
         private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
         {
-            CloseCalloutPopup();
+            // Close callout popup.
+            CalloutImage.Source = null;
+            CalloutPopup.IsVisible = false;
         }
 
         /// <summary>
@@ -118,6 +103,5 @@ namespace PdfZoomCallout
             }
 #endif
         }
-        #endregion
     }
 }
